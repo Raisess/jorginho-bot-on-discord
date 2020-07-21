b14be10ccb22c9c2eb6caf0501eb31d15a6cb2de
danillu <48914278+Raisess@users.noreply.github.com>
2020-07-21 19:56:02 -0300

    created api command fetch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Client } from 'discord.js';
+import fetch from 'node-fetch';
+
 import { bot_token } from './credencials.json';
 
 import { Command, command } from './command';
@@ -9,6 +11,9 @@ const CMD_PREFIX: string = '!';
 
 // setup all bot commands
 const commands: Array<Command> = command();
+
+// bot API uri
+const uri: string = 'http://localhost:1939';
 
 client.once('ready', (): boolean => {
 	console.log('jorginho bot is ready yaaah!');
@@ -30,13 +35,29 @@ client.on('message', (message: any): void => {
 
 	if (message.content.startsWith(CMD_PREFIX)){
 		const args: Array<string> = message.content.slice(1).trim().split(' ');
-		console.log(args);
+		// console.log(args);
 
 		for (let _cmd of commands) {
 			if (args[0] == _cmd.cmd) {
 				return _cmd.func({ message: message, args: args.slice(1) });
 			}
 		}
+
+		// get commands from API
+		(async (message: any, args: Array<string>) => {
+			const guild: string = message.guild.name.toLowerCase().replace(/\s+/g, '_');
+			// api fetch
+			const response = await fetch(`${uri}/command/get/${guild}/${args[0]}`);
+			const data = await response.json();
+
+			if (await data.success) {
+				const messageToSend = await data.command.message;
+
+				return message.channel.send(messageToSend);
+			}
+
+			return;
+		})(message, args);
 	}
 });
 

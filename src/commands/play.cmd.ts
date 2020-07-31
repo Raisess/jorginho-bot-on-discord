14b1e43ea70b327fbb2d637ec44e12ad8574f0af
danillu <48914278+Raisess@users.noreply.github.com>
2020-07-31 20:18:48 -0300

    updated play command to search musics bu the name

--- src/commands/play.cmd.ts
+++ src/commands/play.cmd.ts
@@ -1,67 +1,94 @@
 import { Client } from 'discord.js';
 import ytdl from 'ytdl-core';
+import yts from 'yt-search';
 
 export const play = async (message: any, args: Array<string> | undefined, client: Client): Promise<boolean | any> => {
-	const streamOptions = { volume: 1 };
 	const voiceChannel = message.member.voice.channel;
-	const connection = await voiceChannel.join();
 
-	const music: string = args ? args[0] : '';
+	const music: string = args ? args.join(' ') : '';
 	let musicId: Array<string>;
 
-	if (music.split('/').indexOf('youtu.be') != -1) {
-		musicId = ['cell', music.split('be/')[1]];
-	} else {
-		musicId = ['pc', music.split('=')[1]];
-	}
+	console.log('joined channel');
 
-  console.log('joined channel');
+	if (music.startsWith('https://')) {
+		if (music.split('/').indexOf('youtu.be') != -1) {
+			musicId = ['cell', music.split('be/')[1]];
+		} else {
+			musicId = ['pc', music.split('=')[1]];
+		}
 
-	if (music == 'stop') {
-		console.log('sopping music');
-		voiceChannel.leave();
-
-		message.channel.send(`Tá bem <@${message.author.id}>, eu paro chatx`);
-
-		return true;
-	} else {
 		console.log('trying to play:', music, 'link type:', musicId[0]);
 
-  	const stream = await ytdl(music, { filter: 'audioonly', quality: 'lowestaudio' });
-		const info = await ytdl.getInfo(musicId[1]);
-  	const dispatcher = connection.play(stream, streamOptions);
+		return playMusic(message, client, music, musicId, voiceChannel);
+	} else {
+		if (music == 'stop') {
+			console.log('sopping music');
+			voiceChannel.leave();
 
-		const musicName: string = info.player_response.videoDetails.title;
-
-		dispatcher.on('start', () => {
-			console.log(musicName, 'is now playing!');
-			// console.log('info:', info.player_response.videoDetails);
-
-			client.user.setPresence({
-				status: 'online',
-				activity: {
-					name: musicName,
-					type: 'LISTENING'
-				}
-			});
-		});
-
-  	dispatcher.on('finish', (end: any) => {
-    	console.log('left channel');
-    	voiceChannel.leave();
-
-			message.channel.send('Cabou a música, põe outra ae cria');
+			message.channel.send(`Tá bem <@${message.author.id}>, eu paro chatx`);
 
 			return true;
-  	});
+		} else {
+			yts(music, async (err: any, res: any): Promise<boolean> => {
+				const videos: Array<any> = await res.videos;
+				const videoUrl: string = await videos[0].url;
 
-		dispatcher.on('error', (err: any) => {
-			console.error(err);
+				musicId = ['pc', await videoUrl.split('=')[1]];
 
-			message.channel.send('Eii boy a música miou aqui, e agr???');
+				console.log('trying to play:', videoUrl, 'link type:', musicId[0]);
 
-			return false;
-		});
+				return playMusic(message, client, videoUrl, musicId, voiceChannel);
+			});
+		}
 	}
+}
+
+const playMusic = async (message: any, client: Client, music: string, musicId: Array<string>, voiceChannel: any): Promise<boolean | any> => {	
+	const streamOptions = { volume: 1 };
+	const stream = await ytdl(music, { filter: 'audioonly', quality: 'lowestaudio' });
+	const info = await ytdl.getInfo(musicId[1]);
+ 	
+	const connection = await voiceChannel.join();
+	const dispatcher = connection.play(stream, streamOptions);
+
+	const musicName: string = info.player_response.videoDetails.title;
+
+	dispatcher.on('start', () => {
+		console.log(musicName, 'is now playing!');
+		// console.log('info:', info.player_response.videoDetails);
+
+		client.user.setPresence({
+			status: 'online',
+			activity: {
+				name: musicName,
+				type: 'LISTENING'
+			}
+		});
+	});
+
+ 	dispatcher.on('finish', (end: any) => {
+   	console.log('left channel');
+   	voiceChannel.leave();
+
+		message.channel.send('Cabou a música, põe outra ae cria');
+
+		client.user.setPresence({
+			status: 'online',
+			activity: {
+				name: 'Youtube',
+				type: 'WATCHING'
+			}
+		});
+
+		return true;
+ 	});
+
+	dispatcher.on('error', (err: any) => {
+		console.error(err);
+
+		message.channel.send('Eii boy a música miou aqui, e agr???');
+
+		return false;
+	});
 }
 

04ade6cee9985d7d35002547a78c1f8fe78eec04
danillu <48914278+Raisess@users.noreply.github.com>
2020-08-01 21:51:56 -0300

    added translate to img command

--- src/commands/img.cmd.ts
+++ src/commands/img.cmd.ts
@@ -1,10 +1,14 @@
 import fetch from 'node-fetch';
+import translate from '../modules/translate';
 
 export const img = async (message: any, args: Array<string> | undefined): Promise<void> => {
 	const apiKey: string = '15769415-02ee5908b6583da7cd7ae5fdb';
-	const search: string = args ? args.join('+').toLowerCase() : '';
+	const search: string = args ? args.join(' ').toLowerCase() : '';
 
-	const request = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${search}`);
+	const translateSearch: string = await translate(search, 'por', 'eng');
+	const removeSearchSpaces: string = translateSearch.replace(/\s+/g, '+');
+
+	const request = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${removeSearchSpaces}`);
 	const response = await request.json();
 
 	const allImages: Array<any> = await response.hits;

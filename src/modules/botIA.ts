83b3d4b33bcb775193592597e51e6ea90e71c903
danillu <48914278+Raisess@users.noreply.github.com>
2020-08-16 16:06:01 -0300

    added random-string package

--- src/modules/botIA.ts
+++ src/modules/botIA.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import randomString from 'random-string';
 
 export const conversation = async (question: string): Promise<string> => {
 	const request = await fetch('https://botmake.io/jorginho/reply', {
@@ -23,11 +24,20 @@ export const conversation = async (question: string): Promise<string> => {
 }
 
 export const learn = async (question: string, response: string): Promise<boolean> => {
+	const boundaryToken: string = randomString({
+		length:  16,
+		numeric: true,
+		letters: true,
+		special: false
+	});
+
+	console.log(boundaryToken);
+
 	try {
 		const request = await fetch('https://botmake.io/admin/answers', {
 			method: 'post',
 			headers: {
-				'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary05T2srNzq7shBuBp',
+				'Content-Type': `multipart/form-data; boundary=----WebKitFormBoundary${boundaryToken}`,
 				cookie: '__zlcmid=zTjKLp999kj7fE; kt_aside_menu=0; XSRF-TOKEN=eyJpdiI6InNsdU9tNUltdkw1eGpNcEYwNHdrV2c9PSIsInZhbHVlIjoiVzVORUFFUXdjM1JlSmdOekg2bjQwSFNBQUVtK0UzYVhHN2NuYmFrQ1R6VjBVTm5OREFzdmQzTEFDVk1laWpnQyIsIm1hYyI6IjYyODEwMGI2NmY3OTExYzkzMDI3YjI2NzRmMzFkNGIxMDFmNmJmZjAxZWY1MWJmYmIzZDUwMWQ3MmQ2MGY3NjUifQ%3D%3D; botmakeio_session=eyJpdiI6ImRFRmg3TE5HMFhJZ2dkMSs1T1VxMVE9PSIsInZhbHVlIjoiTUNEWlwvaWNsdGEyaUlKbTB5RkNFK0NGbmg1blpUNUlwWVB6REV6MGpJeW92b25wWmJJY1ZqNUgzNmhzaHZ0Y3ciLCJtYWMiOiI2YzdhMWQ3MTFiOGM5NjU4ODgxNDljZWQ4ZWY4MTE3MjIwZjMwODY5NjZjZGIxZDVjNTk1Y2MyM2E5NWExYmE1In0%3D',
 				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/53',
 				connection: 'keep-alive',
@@ -39,31 +49,33 @@ export const learn = async (question: string, response: string): Promise<boolean
 				'Sec-Fetch-Mode': 'cors',
 				'Sec-Fetch-Site': 'same-origin',
 				origin: 'https://botmake.io',
-				referer: 'https://botmake.io/jorginho',
+				referer: 'https://botmake.io/admin/teach',
 				'X-CSRF-TOKEN': 'FGBsjKGi4edYXT8buLfIaWV1mfgZHu3Bd6W8Xa8P',
 				'X-Requested-With': 'XMLHttpRequest'
 			},
-			body: `------WebKitFormBoundary05T2srNzq7shBuBp
+			body: `
+				------WebKitFormBoundary${boundaryToken}
 				Content-Disposition: form-data; name="media"
 
 				undefined
-				------WebKitFormBoundary05T2srNzq7shBuBp
+				------WebKitFormBoundary${boundaryToken}
 				Content-Disposition: form-data; name="questionId"
 
 
-				------WebKitFormBoundary05T2srNzq7shBuBp
+				------WebKitFormBoundary${boundaryToken}
 				Content-Disposition: form-data; name="answerText"
 
 				${response}
-				------WebKitFormBoundary05T2srNzq7shBuBp
+				------WebKitFormBoundary${boundaryToken}
 				Content-Disposition: form-data; name="type"
 
 				text
-				------WebKitFormBoundary05T2srNzq7shBuBp
+				------WebKitFormBoundary${boundaryToken}
 				Content-Disposition: form-data; name="questionText"
 
 				${question}
-				------WebKitFormBoundary05T2srNzq7shBuBp--`,
+				------WebKitFormBoundary${boundaryToken}--
+			`,
 		});
 
 		const responseStatus:  number = await request.status;

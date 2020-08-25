1ae4f8f36f799661fe4f4dfae34b46adb3b2ecb6
danillu <48914278+Raisess@users.noreply.github.com>
2020-08-25 10:41:16 -0300

    created a count message replace in message engine

--- src/messageEngine.ts
+++ src/messageEngine.ts
@@ -8,10 +8,54 @@ export interface MessageEngineCommand {
   message:     string;
 }
 
+interface ICount {
+	message:  string;
+	count:    number;
+}
+
+let counts: Array<ICount> = [];
+
 export const messageEngine = (_message: any, messageData: MessageEngineCommand | any): string => {
 	const { creator_id, message } = messageData;
 	// replace words
-	const customMessage: string = message.replace(/({username})/g, `<@${_message.author.id}>`).replace(/({me})/g, `<@${creator_id}>`);
+	let customMessage: string = message.replace(/({username})/g, `<@${_message.author.id}>`)
+		.replace(/({me})/g, `<@${creator_id}>`);
+
+	let splitedCustomMessage: Array<string> = customMessage.split(' ');
+
+	// check if a count message command
+	if (splitedCustomMessage.indexOf('{count}') != -1) {
+		// if not have a initialize, initialize this shit
+		if (counts.length != 0) {
+			for (let i = 0; i < counts.length; i++) {
+				// if have a initialize update that
+				if (counts[i].message == customMessage) {
+					counts[i].count = counts[i].count + 1;
+
+					// normal replace is not working when i try to replace {cout}, but i make this "gambiarra" :rage:
+					splitedCustomMessage[splitedCustomMessage.indexOf('{count}')] = String(counts[i].count);
+					return splitedCustomMessage.join(' ');
+				}
+			}
+
+			// initilize a anothe message if the count array length != 0
+			counts.push({
+				message: customMessage,
+				count:   1
+			});
+
+			splitedCustomMessage[splitedCustomMessage.indexOf('{count}')] = '1';
+			return splitedCustomMessage.join(' ');
+		} else {
+			counts.push({
+				message: customMessage,
+				count:   1
+			});
+
+			splitedCustomMessage[splitedCustomMessage.indexOf('{count}')] = '1';
+			return splitedCustomMessage.join(' ');
+		}
+	}
 
 	return customMessage;
 }

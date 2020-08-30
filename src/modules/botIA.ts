911620e6072cff29653c32ac00b8c98affe53da1
danillu <48914278+Raisess@users.noreply.github.com>
2020-08-30 09:52:38 -0300

    updated bot conversation module

--- src/modules/botIA.ts
+++ src/modules/botIA.ts
@@ -1,27 +1,19 @@
 import fetch from 'node-fetch';
+import { uri } from '../credencials.json';
 
 export const conversation = async (question: string): Promise<string> => {
-	const request = await fetch('https://botmake.io/jorginho/reply', {
+	const request = await fetch(`${uri}/conversation/talk`, {
 		method: 'post',
 		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-			cookie: '__zlcmid=zTjKLp999kj7fE; kt_aside_menu=0; XSRF-TOKEN=eyJpdiI6InNsdU9tNUltdkw1eGpNcEYwNHdrV2c9PSIsInZhbHVlIjoiVzVORUFFUXdjM1JlSmdOekg2bjQwSFNBQUVtK0UzYVhHN2NuYmFrQ1R6VjBVTm5OREFzdmQzTEFDVk1laWpnQyIsIm1hYyI6IjYyODEwMGI2NmY3OTExYzkzMDI3YjI2NzRmMzFkNGIxMDFmNmJmZjAxZWY1MWJmYmIzZDUwMWQ3MmQ2MGY3NjUifQ%3D%3D; botmakeio_session=eyJpdiI6ImRFRmg3TE5HMFhJZ2dkMSs1T1VxMVE9PSIsInZhbHVlIjoiTUNEWlwvaWNsdGEyaUlKbTB5RkNFK0NGbmg1blpUNUlwWVB6REV6MGpJeW92b25wWmJJY1ZqNUgzNmhzaHZ0Y3ciLCJtYWMiOiI2YzdhMWQ3MTFiOGM5NjU4ODgxNDljZWQ4ZWY4MTE3MjIwZjMwODY5NjZjZGIxZDVjNTk1Y2MyM2E5NWExYmE1In0%3D',
-			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/53',
-			connection: 'keep-alive',
-			origin: 'https://botmake.io',
-			referer: 'https://botmake.io/jorginho',
-			'X-CSRF-TOKEN': 'FGBsjKGi4edYXT8buLfIaWV1mfgZHu3Bd6W8Xa8P',
-			'X-Requested-With': 'XMLHttpRequest'
+			'Content-Type': 'application/json',
 		},
-		body: `question=${encodeURI(question)}`,
+		body: JSON.stringify({
+			question: question
+		})
 	});
 
 	const response = await request.json();
-	const text: string = await response.text;
-
-	if (text.startsWith("Question can't be longer than 100 chars.")) {
-		return 'Ou isso é um link ou é um textão, vou ler nada saporra...';
-	}
+	const text: string = await response.message;
 
 	return text;
 }

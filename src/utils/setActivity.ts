19c552df4e064a2e5343fe4951397206b4d4e5f9
danillu <48914278+Raisess@users.noreply.github.com>
2020-09-01 13:43:17 -0300

    updated setActivity func params type

--- src/utils/setActivity.ts
+++ src/utils/setActivity.ts
@@ -1,9 +1,6 @@
 import { Client } from 'discord.js';
 
-export declare type Activity = 'WATCHING' | 'PLAYING' | 'LISTENING' | 'STREAMING' | undefined;
-export declare type Status = 'online' | 'idle' | 'invisible' | 'dnd' | undefined;
-
-export const setActivity = (client: Client, status: Status, name: string, type: Activity): boolean => {
+export const setActivity = (client: Client, status: string, name: string, type: string): boolean => {
 	// setting bot activity
 	client.user.setPresence({
 	  status: status,

9989ed1a49ca5bdafbc7fe59fded31af6b3446c1
danillu <48914278+Raisess@users.noreply.github.com>
2020-09-09 11:34:47 -0300

    updated netflix(movie indication commmand) message returns to a embed

--- src/commands/netflix.cmd.ts
+++ src/commands/netflix.cmd.ts
@@ -1,8 +1,11 @@
+import { MessageEmbed } from 'discord.js';
 import fetch from 'node-fetch';
+
 import translate from '../modules/translate';
+import { colors } from '../utils/colors';
 
 export const indication_ = async (message: any): Promise<void> => {
-	const request = await fetch('https://api.reelgood.com/v3.0/content/roulette?content_kind=both&free=false&nocache=true&region=us&sources=netflix%2Chulu_plus%2Camazon_prime%2Cdisney_plus%2Chbo_max%2Chbo%2Cpeacock%2Capple_tv_plus%2Cfubo_tv%2Cshowtime%2Cstarz%2Ccbs_all_access%2Cepix%2Ccrunchyroll_premium%2Cfunimation%2Camc_premiere%2Ckanopy%2Choopla%2Ccriterion_channel%2Cbritbox%2Cdc_universe%2Cmubi%2Ccinemax%2Cfandor%2Cacorntv%2Challmark_movies_now%2Cbet_plus%2Cshudder%2Cyoutube_premium%2Cindieflix');
+	const request = await fetch('https://api.reelgood.com/v3.0/content/roulette?content_kind=both&free=false&nocache=true&region=us&sources=netflix%2Camazon_prime%2Cdisney_plus%2Chbo_max%2Chbo%2Cfubo_tv%2Ccrunchyroll_premium%2Cfunimation%2Camc_premiere%2Cdc_universe%2Cmubi%2Ccinemax');
 	const response = await request.json();
 	
 	const contents: any = {
@@ -22,8 +25,16 @@ export const indication_ = async (message: any): Promise<void> => {
 	const imgUrl: string = `https://img.reelgood.com/content/${indication.contentType[0]}/${indication.id}/poster-780.webp`;
 	const translatedDesc: string = await translate(indication.description, 'eng', 'por');
 
-	const text: string = `🎰 ROLETA DA INDICAÇÃO 🎬\nTitulo: ${indication.title}\nTipo: ${indication.contentType[1]}\nDescrição: ${translatedDesc}\nClassificação: ${indication.classification}\nNota no IMDB: ${indication.imdbRating}\n${imgUrl}`;
+	const embed: MessageEmbed = new MessageEmbed()
+		.setColor(colors[Math.round(Math.random() * colors.length) - 1])
+		.setTitle('🎰 ROLETA DA INDICAÇÃO 🎬')
+		.setImage(imgUrl)
+		.addField('Titulo:', indication.title)
+		.addField('Descrição:', translatedDesc)
+		.addField('Nota no IMDB:', indication.imdbRating, true)
+		.addField('Tipo:', indication.contentType, true)
+		.addField('Classificação indicativa', indication.classification, true);
 
-	return message.channel.send(text);
+	return message.channel.send(embed);
 }
 

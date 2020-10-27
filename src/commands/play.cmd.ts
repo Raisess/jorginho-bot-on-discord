0dbcb40180c67c822bcf5e2495f50befc6dbb53f
Raisess <raisechif@gmail.com>
2020-10-27 12:09:48 -0300

    created music queue

--- src/commands/play.cmd.ts
+++ src/commands/play.cmd.ts
@@ -1,116 +1,156 @@
-import { Client, MessageEmbed } from 'discord.js';
-import ytdl from 'ytdl-core-discord';
-import yts from 'yt-search';
+import { Client, MessageEmbed } from "discord.js";
+import ytdl from "ytdl-core-discord";
+import yts from "yt-search";
 
-import { colors } from '../utils/colors';
-import { setActivity } from '../utils/setActivity';
+import { colors } from "../utils/colors";
+import { setActivity } from "../utils/setActivity";
+
+interface IMusic {
+	id:     string;
+	url:    string;
+	search: string;
+}
+
+let queue: Array<IMusic> = [];
 
 export const play = async (message: any, args: Array<string> | undefined, client: Client): Promise<boolean | any> => {
 	const voiceChannel: any = message.member.voice.channel;
 
-	const music: string = args ? args.join(' ') : '';
-	let musicId: Array<string>;
+	const music: string        = args ? args.join(" ") : "";
+	let musicId: Array<string> = ["", ""];
 
-	if (music != 'stop') {
-		message.channel.send(`Calma tô caçando aqui essa música: ${music}...`);
+	if (music != "stop" && music != "skip") {
+		message.channel.send(`Música adicionada a fila: ${music}`);
 	}
 
 	// play music with direct link
-	if (music.startsWith('https://')) {
-		if (music.split('/').indexOf('youtu.be') != -1) {
-			musicId = ['cell', music.split('be/')[1]];
+	if (music.startsWith("https://")) {
+		if (music.split("/").indexOf("youtu.be") != -1) {
+			musicId = ["cell", music.split("be/")[1]];
 		} else {
-			musicId = ['pc', music.split('=')[1]];
+			musicId = ["pc", music.split("=")[1]];
 		}
 
-		return playMusic(message, client, music, musicId, voiceChannel);
+		console.log("music added:", music);
+		
+		queue.push({
+			id:     musicId[1],
+			url:    music,
+			search: music
+		});
+
+		console.log("music queue:", queue);
+	
+		if (queue.length == 1) {
+			return playMusic(message, client, queue[0].url, queue[0].id, voiceChannel);
+		}
+
+		return true;
 	} else {
 		// play music with name
-		if (music == 'stop') {
+		if (music == "stop") {
 			voiceChannel.leave();
 			message.channel.send(`Tá bem <@${message.author.id}>, eu paro chatx`);
 
-			setActivity(client, 'online', 'Youtube', 'WATCHING');
+			setActivity(client, "online", "Youtube", "WATCHING");
 
 			return true;
+		} else if (music == "skip") {
+			queue.shift();
+
+			console.log("music queue:", queue);
+		
+			return playMusic(message, client, queue[0].url, queue[0].id, voiceChannel);
 		} else {
-			console.log('music:', music);
+			console.log("music added:", music);
 
-			const res: any = await yts(music);
-			const videos: Array<any> = await res.videos;
-			const videoUrl: string = await videos[0].url;
+			const res:      any        = await yts(music);
+			const videos:   Array<any> = await res.videos;
+			const videoUrl: string     = await videos[0].url;
 
-			musicId = ['pc', await videoUrl.split('=')[1]];
+			musicId = ["pc", await videoUrl.split("=")[1]];
 
-			return playMusic(message, client, videoUrl, musicId, voiceChannel);
+			queue.push({
+				id:     musicId[1],
+				url:    videoUrl,
+				search: music
+			});
+
+			console.log("music queue:", queue);
 		}
 	}
+
+	if (queue.length == 1) {
+		return playMusic(message, client, queue[0].url, queue[0].id, voiceChannel);
+	}
+
+	return true;
 }
 
-const playMusic = async (message: any, client: Client, music: string, musicId: Array<string>, voiceChannel: any): Promise<boolean | any> => {	
+const playMusic = async (message: any, client: Client, music: string, musicId: string, voiceChannel: any): Promise<boolean | any> => {	
 	try {
-		const streamOptions = {
-			volume: false,
-			type:   'opus',
-			highWaterMark: 10
-		};
-
-		const stream: any = await ytdl(music, {
-			filter:  'audioonly',
-			quality: 'highestaudio'
-		});
-
-		const info: any = await ytdl.getInfo(musicId[1]);
  		// voice connection
 		const connection: any = await voiceChannel.join();
-		const dispatcher: any = await connection.play(stream, streamOptions);
+		const dispatcher: any = await connection.play(await ytdl(music), { type: "opus" });
 
 		// music info
+		const info:         any = await ytdl.getInfo(musicId);
 		const videoDetails: any = info.player_response.videoDetails;
 		const musicName: string = videoDetails.title;
 		const thumbnail: string = videoDetails.thumbnail.thumbnails[3].url;
 		const author:    string = videoDetails.author;
 		const views:     string = videoDetails.viewCount;
-		const duration:  string = String((parseInt(videoDetails.lengthSeconds) / 60).toFixed(2)).replace(/\./g, ':');
-
-		//console.log(duration);
+		const duration:  string = String((parseInt(videoDetails.lengthSeconds) / 60).toFixed(2)).replace(/\./g, ":");
 
 		// on music starts
-		dispatcher.on('start', () => {
-			setActivity(client, 'online', musicName, 'LISTENING');
+		dispatcher.on("start", () => {
+			setActivity(client, "online", musicName, "LISTENING");
 
 			const embed: MessageEmbed = new MessageEmbed()
 				.setColor(colors[Math.round(Math.random() * colors.length) - 1])
 				.setTitle(musicName)
 				.setURL(music)
 				.setImage(thumbnail)
-				.addField('Tocando:', musicName)
-				.addField('Autor:', author)
-				.addField('Visualizações:', views, true)
-				.addField('Duração:', duration, true);
+				.addField("Tocando:", musicName)
+				.addField("Autor:", author)
+				.addField("Visualizações:", views, true)
+				.addField("Duração:", duration, true);
 			
 			message.channel.send(embed);
 		});
 
 		// on music ends
- 		dispatcher.on('finish', (end: any) => {
+ 		dispatcher.on("finish", (end: any) => {
+			queue.shift();
+
+			if (queue.length > 0) {
+				return playMusic(message, client, queue[0].url, queue[0].id, voiceChannel);
+			}
+
    		voiceChannel.leave();
-			message.channel.send('Cabou a música, põe outra ae cria');
-			setActivity(client, 'online', 'Youtube', 'WATCHING');
+			message.channel.send("Cabou a música, põe outra ae cria");
+			setActivity(client, "online", "Youtube", "WATCHING");
 
 			return true;
  		});
 
 		// on music error
-		dispatcher.on('error', (err: any) => {
+		dispatcher.on("error", (err: any) => {
+			queue.shift();
+
+			if (queue.length > 0) {
+				return playMusic(message, client, queue[0].url, queue[0].id, voiceChannel);
+			}
+
    		voiceChannel.leave();
-			message.channel.send('Eii boy a música miou aqui, e agr???');
-			setActivity(client, 'online', 'Youtube', 'WATCHING');
+			message.channel.send("Eii boy a música miou aqui, e agr???");
+			message.channel.send(err.toString(), { code: "xl" });
+			setActivity(client, "online", "Youtube", "WATCHING");
 
 			return false;
 		});
 	} catch (e) {
-		message.channel.send('Eu não sou nenhum gênio ou algo parecido, mas aparentemente você tem q estar no voice pra me ouvir não acha?');
+		message.channel.send(e.message, { code: "xl" });
 
 		return true;
 	}
